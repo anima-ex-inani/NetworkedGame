eac1a8438562d02fed5662e7c915f96b3d4321cf
PerseverantDT <perseverant-determination@outlook.com>
2026-03-30 00:25:02 +0800

    Completely remove JavaFX dependencies

--- game/src/main/java/module-info.java
+++ game/src/main/java/module-info.java
@@ -2,6 +2,5 @@ module io.github.animaexinani.game {
     requires transitive org.jetbrains.annotations;
 
     requires io.github.animaexinani.engine;
-    requires javafx.graphics;
     requires java.logging;
 }
