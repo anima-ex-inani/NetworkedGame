438d8213e754ed70725925c4cbb4a38ec38edc29
dlocsin13 <dalocsin@up.edu.ph>
2026-04-23 20:54:07 +0800

    refactor: refactored implementations to match new drawable

--- engine/src/main/java/io/github/animaexinani/engine/rendering/drawable/Geometry.java
+++ engine/src/main/java/io/github/animaexinani/engine/rendering/drawable/Geometry.java
@@ -9,40 +9,8 @@ import java.util.Objects;
 
 public final class Geometry implements Drawable {
     private @NotNull Vertex @NotNull [] vertices;
-
-    @Override
-    public Vertex @NotNull [] vertices() {
-        return this.vertices;
-    }
-
-    public void vertices(@NotNull Vertex @NotNull [] vertices) {
-        Objects.requireNonNull(vertices);
-        for (Vertex vertex : vertices) {
-            Objects.requireNonNull(vertex);
-        }
-        this.vertices = vertices;
-    }
-
     private int @NotNull [] indices;
-
-    public int @NotNull [] indices() {
-        return this.indices;
-    }
-
-    public void indices(int @NotNull [] indices) {
-        Objects.requireNonNull(indices);
-        this.indices = indices;
-    }
-
     private @Nullable Texture texture;
-
-    public @Nullable Texture texture() {
-        return this.texture;
-    }
-
-    public void texture(@Nullable Texture texture) {
-        this.texture = texture;
-    }
 
     public Geometry(@NotNull Vertex @NotNull [] vertices, int @NotNull [] indices, @Nullable Texture texture) {
         Objects.requireNonNull(vertices);
@@ -53,6 +21,60 @@ public final class Geometry implements Drawable {
 
         this.vertices = vertices;
         this.indices = indices;
+        this.texture = texture;
+    }
+
+    // --- Drawable ---
+
+    @Override
+    public int vertexCount() {
+        return this.vertices.length;
+    }
+
+    @Override
+    public @NotNull Vertex vertexAt(int index) {
+        if (index < 0 || index >= this.vertices.length) {
+            throw new IndexOutOfBoundsException(
+                    "Vertex index " + index + " out of bounds for length " + this.vertices.length);
+        }
+        return this.vertices[index];
+    }
+
+    @Override
+    public int indexCount() {
+        return this.indices.length;
+    }
+
+    @Override
+    public int indexAt(int index) {
+        if (index < 0 || index >= this.indices.length) {
+            throw new IndexOutOfBoundsException(
+                    "Index " + index + " out of bounds for length " + this.indices.length);
+        }
+        return this.indices[index];
+    }
+
+    @Override
+    public @Nullable Texture texture() {
+        return this.texture;
+    }
+
+    // --- Setters ---
+
+    public void setVertices(@NotNull Vertex @NotNull [] vertices) {
+        Objects.requireNonNull(vertices);
+        for (Vertex vertex : vertices) {
+            Objects.requireNonNull(vertex);
+        }
+        this.vertices = vertices;
+    }
+
+    public void setIndices(int @NotNull [] indices) {
+        Objects.requireNonNull(indices);
+        this.indices = indices;
+    }
+
+    public void setTexture(@Nullable Texture texture) {
         this.texture = texture;
     }
 }
