ee9b9816f397161c61d869543ba8dc26ba24a775
PerseverantDT <perseverant-determination@outlook.com>
2026-05-10 22:10:45 +0800

    Remove missing module from module manifest

--- engine/src/main/java/module-info.java
+++ engine/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module io.github.animaexinani.engine {
     requires org.lwjgl;
     requires org.lwjgl.sdl;
-    requires org.dyn4j;
     
     requires transitive org.jetbrains.annotations;
     exports io.github.animaexinani.engine;
