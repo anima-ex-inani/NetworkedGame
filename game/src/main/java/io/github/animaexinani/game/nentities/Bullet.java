557b1645b9861e9dbe220bcbf3d94231b33cfc0b
PerseverantDT <perseverant-determination@outlook.com>
2026-05-14 13:32:55 +0800

    Update Bullet to use the DealsContactDamage interface

--- game/src/main/java/io/github/animaexinani/game/nentities/Bullet.java
+++ game/src/main/java/io/github/animaexinani/game/nentities/Bullet.java
@@ -1,33 +1,15 @@
 package io.github.animaexinani.game.nentities;
 
-import java.util.Set;
-
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * Represents a bullet within the game's playfield
  */
-public interface Bullet extends Entity {
+public interface Bullet extends DealsContactDamage {
     /**
      * Gets the owner of this bullet
      * 
-     * @return The bullet's owner
+     * @return The bullet's owner, or null if the bullet has no owner
      */
     @Nullable Entity owner();
-
-    /**
-     * Gets the damage of this bullet
-     * 
-     * @return The bullet's damage
-     */
-    int damage();
-
-    /**
-     * Gets the entity types that this bullet should ignore. If the bullet collides
-     * with an entity of one of these types, it will not deal damage.
-     * 
-     * @return The entity types that this bullet should ignore
-     */
-    @NotNull Set<@NotNull EntityType> ignoredEntities();
 }
