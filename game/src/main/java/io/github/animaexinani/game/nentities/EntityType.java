184ccf3e0d81aa10a76a81de9681b636abcf3b8e
PerseverantDT <perseverant-determination@outlook.com>
2026-05-14 11:15:31 +0800

    Add helper functions to determine the category of an entity type

--- game/src/main/java/io/github/animaexinani/game/nentities/EntityType.java
+++ game/src/main/java/io/github/animaexinani/game/nentities/EntityType.java
@@ -16,5 +16,47 @@ public enum EntityType {
     /**
      * The entity refers to an asteroid.
      */
-    ASTEROID,
+    ASTEROID;
+
+    /**
+     * Checks if the entity type refers to a player entity.
+     * @return true if the entity type is PLAYER, false otherwise
+     */
+    public boolean player() {
+        return this == PLAYER;
+    }
+
+    /**
+     * Checks if the entity type refers to an obstacle entity.
+     * @return true if the entity type is ASTEROID, false otherwise
+     */
+    public boolean obstacle() {
+        return this.asteroid();
+    }
+
+    /**
+     * Checks if the entity type refers to an asteroid entity.
+     * @return true if the entity type is ASTEROID, false otherwise
+     */
+    public boolean asteroid() {
+        return this == ASTEROID;
+    }
+
+    /**
+     * Checks if the entity type refers to a bullet entity.
+     * @return Currently always returns false until bullets are implemented
+     */
+    public boolean bullet() {
+        // TODO: Change the implementation when bullets are added
+        return false;
+    }
+
+    /**
+     * Checks if the entity type refers to an enemy entity.
+     * @return Currently always returns false until enemies are implemented
+     */
+    public boolean enemy() {
+        // TODO: Change the implementation when enemies are added
+        return false;
+    }
 }
