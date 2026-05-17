bc886d555986fb5a7436a9999b8768bbe12fcc98
PerseverantDT <perseverant-determination@outlook.com>
2026-05-17 14:33:27 +0800

    Add documentation to Texture

--- engine/src/main/java/io/github/animaexinani/engine/texture/Texture.java
+++ engine/src/main/java/io/github/animaexinani/engine/texture/Texture.java
@@ -5,15 +5,27 @@ import io.github.animaexinani.engine.point.PointF;
 import io.github.animaexinani.engine.size.Size;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents a texture that has been loaded into GPU memory.
+ */
 public interface Texture extends AutoCloseable {
+    /**
+     * @return The size of the texture.
+     */
     @NotNull
     Size getSize();
 
+    /**
+     * Gets the UV coordinates of the top-left corner of a pixel in the texture.
+     * 
+     * @param point The pixel to get the UV coordinates of.
+     * @return The UV coordinates of the pixel.
+     */
     @NotNull
     default PointF getUvOfPoint(@NotNull Point point) {
         var size = this.getSize();
-        var u = (float)point.x() / (float)size.width();
-        var v = (float)point.y() / (float)size.height();
+        var u = (float) point.x() / (float) size.width();
+        var v = (float) point.y() / (float) size.height();
         return new PointF(u, v);
     }
 }
