b17dbd4461f6c9764f750a97747e255c45b8668d
PerseverantDT <perseverant-determination@outlook.com>
2026-05-19 08:57:40 +0800

    Add implementation specifications to Drawable.draw

--- engine/src/main/java/io/github/animaexinani/engine/rendering/drawable/Drawable.java
+++ engine/src/main/java/io/github/animaexinani/engine/rendering/drawable/Drawable.java
@@ -11,6 +11,11 @@ public interface Drawable {
      * Draws the object to the specified rendering context.
      *
      * @param context the context to draw to
+     * 
+     * @implSpec Due to `context` being shared across all drawables, there is no
+     *           guarantee as to the initial state of the render context when
+     *           this function is called. You must initialize the context to
+     *           your expected state before you use it.
      */
     void draw(@NotNull RenderContext context);
 }
